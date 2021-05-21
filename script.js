2ec808bc87b707257aef283c2cb4df3df37a9520
Patryk Jaworski <pjaworski.dev@gmail.com>
2021-05-21 03:40:27 +0200

    Add easy-to-close logic & add comments

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
-let navSearch = document.getElementById('nav-search');
-let navSearchBtn = document.getElementById('nav-search-btn');
-let navSearchInput = document.getElementById('nav-search-block');
-let moreBtn = document.getElementById('more-nav');
-let listBg = document.querySelector('.more-nav');
-let moreMenu = document.querySelector('.more-nav__list');
-let mainNav = document.querySelector('.main-nav');
+let navSearch = document.getElementById('nav-search'); // родитель поиска
+let navSearchBtn = document.getElementById('nav-search-btn'); //кнопка поиска
+let navSearchInput = document.getElementById('nav-search-block'); //поле поиска
+let moreBtn = document.getElementById('more-nav'); //кнопка доп меню
+let listBg = document.querySelector('.more-nav'); //бекграунд доп меню
+let moreMenu = document.querySelector('.more-nav__list'); //список доп меню
+let mainNav = document.querySelector('.main-nav'); //основное меню
+let headerNav = document.querySelector('.header-navbar'); //навбар
 
+//появление поля поиска и проверка меню на открытость
 navSearchBtn.onclick = function() {
   if(listBg.classList.contains('more-nav-active')) {
   moreBtnAction();
-  navSearch.classList.add('search-open');
-  navSearch.focus();
-  navSearchInput.focus();
+  searchOpen();
 } else {
-  navSearch.classList.add('search-open');
-  navSearchInput.focus();
-  console.log('1');
+  searchOpen();
 }}
 
+//открытие меню при нажатии на кнопку
 moreBtn.onclick = function() {
   moreBtnAction();
 }
-
+//2 ивента на закрытие элементов при нажатии в любое место
 document.addEventListener('mouseup', function(e) {
     if (!navSearchInput.contains(e.target)) {
     navSearch.classList.remove('search-open');
-    }
+  }
 });
 
+document.addEventListener('mouseup', function(e) {
+    if (!headerNav.contains(e.target)) {
+      moreMenu.classList.remove('more-active');
+      listBg.classList.remove('more-nav-active');
+      mainNav.classList.remove('visuallyhidden');
+  }
+});
+
+//функция поиска
+function searchOpen(){
+  navSearch.classList.add('search-open');
+  navSearchInput.focus();
+}
+
+//функция меню
  function moreBtnAction() {
   moreMenu.classList.toggle('more-active');
   listBg.classList.toggle('more-nav-active');
