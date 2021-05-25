062db007b6bee07195ea55ed9831addc98430c29
Patryk Jaworski <pjaworski.dev@gmail.com>
2021-05-25 11:39:03 +0200

    Add smooth scroll to anchor

--- script.js
+++ script.js
@@ -58,3 +58,41 @@ debugMenuBtn.onclick = function() {
   debugMenuBtn.innerHTML = "+";
 } else {debugMenuBtn.innerHTML = "✕";}
 }
+
+//smooth scroll
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 800);
+});
+
+//smooth scroll iOS Safari
+(function() {
+    scrollTo();
+})();
+
+function scrollTo() {
+    const links = document.querySelectorAll('.scroll');
+    links.forEach(each => (each.onclick = scrollAnchors));
+}
+
+function scrollAnchors(e, respond = null) {
+    const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+    e.preventDefault();
+    var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
+    const targetAnchor = document.querySelector(targetID);
+    if (!targetAnchor) return;
+    const originalTop = distanceToTop(targetAnchor);
+    window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+    const checkIfDone = setInterval(function() {
+        const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+        if (distanceToTop(targetAnchor) === 0 || atBottom) {
+            targetAnchor.tabIndex = '-1';
+            targetAnchor.focus();
+            window.history.pushState('', '', targetID);
+            clearInterval(checkIfDone);
+        }
+    }, 1000);
+}
